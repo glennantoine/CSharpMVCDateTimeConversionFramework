76df62c1301f7e965442df7168d310dfd1cc0bbb
Glenn Antoine <glenn.antoine@drivedominion.com>
2012-11-27 11:38:13 -0500

    continued testing/work with htmlhelpers

---
+++ CustomModelBindingWithDateTime/Helpers/UiDateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace CustomModelBindingWithDateTime.Helpers 
+{
+    public static class UiDateTimeHelper 
+    {
+
+    }
+}

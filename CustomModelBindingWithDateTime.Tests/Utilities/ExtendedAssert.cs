6a889c8d3d71f4258d50f8c1713d6ed91e694b2b
Glenn Antoine <glenn@evsminder.com>
2012-12-12 20:48:52 -0500

    working on more testing options

--- CustomModelBindingWithDateTime.Tests/Utilities/ExtendedAssert.cs
+++ CustomModelBindingWithDateTime.Tests/Utilities/ExtendedAssert.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CustomModelBindingWithDateTime.Utilities
+namespace CustomModelBindingWithDateTime.Tests.Utilities
 {
     public static class ExtendedAssert
     {
